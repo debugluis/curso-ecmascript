b8154c8804d144e6ec358fe4d67dd1f02d62ec6c
mrluisperez <lkperezospino@gmail.com>
2022-12-25 22:49:34 -0600

    ES6: arrays & objects destructuring

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,10 @@
+//arrays destructuring
+let fruits = ['apple','banana'];
+let [a, b] = fruits;
+console.log(a, fruits[1]);
+
+//objets destructuring
+let user = {username: 'luis', age: 32};
+let {username, age} = user;
+
+console.log(username, age);
