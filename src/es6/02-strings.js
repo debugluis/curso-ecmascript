e267571172154fdc36a28e068533eaa3d466c811
mrluisperez <lkperezospino@gmail.com>
2022-12-25 22:28:16 -0600

    ES6: STRINGS

---
+++ src/es6/02-strings.js
@@ -0,0 +1,16 @@
+let hello = 'hello';
+let world = 'world';
+let epicPhrase = hello + ' ' +  world + '!';
+console.log(epicPhrase);
+
+//Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+//MultiLine strings
+let lorem = 'esto es un string\n ' + 'esto es otra linea';
+console.log(lorem);
+
+let lorem2 = `esto es un string
+esto es otra linea`;
+console.log(lorem2);
