170cb4b8ad1352bbd60a9abfa0ced9d2f1ab01af
mrluisperez <lkperezospino@gmail.com>
2022-12-25 22:59:26 -0600

    ES6: spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -6,5 +6,21 @@ console.log(a, fruits[1]);
 //objets destructuring
 let user = {username: 'luis', age: 32};
 let {username, age} = user;
-
 console.log(username, age);
+
+//spreat operator
+let person = {username: 'luis', age: 32};
+let country = 'CO';
+
+let data = {id: 1, ...person, country};
+console.log(data);
+
+//rest
+function sum (num, ...values) {
+    
+console.log(values);
+console.log(num + values[0]);
+return num + values[0];
+}
+
+sum(1, 1, 2, 3);
